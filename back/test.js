5b6ec5fecd3ec4bb5fe9d33491504f14d92f25ac
Beshamel <bpolenor33@gmail.com>
2024-02-28 18:06:40 +0100

    2.0.0

--- back/test.js
+++ back/test.js
@@ -14,7 +14,7 @@ app.use(
     })
 )
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(cors({ origin: 'https://matos.hyris.tv', credentials: true }))
 
 app.post('/login', (req, res) => {
     const username = req.body.username
